455dd5a3761e4dbd594f217e845aae44da086fe4
lionelchoco <72787882+Deric95@users.noreply.github.com>
2021-04-01 17:02:33 +0000

    -re commit @HR_SuperCar-

---
+++ @HR_MoSuperCar/src/Employe/employe.java
@@ -0,0 +1,7 @@
+package Employe;
+
+public class employe {
+
+}
+
+
--- hello/src/hello/Hello.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-public class Hello {
-	  public static void main(String[] args) {
-	        // Prints "Hello, World" to the terminal window.
-	        System.out.println("Hello, World");
-	  }
-}
-  
