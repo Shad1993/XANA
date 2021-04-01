15a5a9626540240ed0e74344ea85e48a5e4a5713
lionelchoco <72787882+Deric95@users.noreply.github.com>
2021-04-01 14:26:30 +0000

    the version.

---
+++ @HR_MoSuperCar/src/com/example/constants/QueryStatement.java
@@ -0,0 +1,21 @@
+package com.example.constants;
+
+public class QueryStatement {
+	
+	public final static String ADD_EMPLOYE = "INSERT INTO employes(Nom,Prenom,NIC,DOB,Sexe,Adresse,AdresseEmail,No_contact,Titre,Salaire,DateDembauche,Comission,Nodept) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)"; 
+	
+	//public final static String UPDATE_EMPLOYE_QUERY = "UPDATE employes SET Nom=?,Prenom=?,NIC=?,DOB=?,Sexe=?,Adresse=?,AdresseEmail=?,No_contact=?,Titre=?,Salaire=?,DateDembauche=?,Comission=?,Nodept=? where No_employe=?";
+	public final static String UPDATE_EMPLOYE = "UPDATE employes SET Nom=?,Prenom=?,NIC=?,DOB=?,Sexe=?,Adresse=?,AdresseEmail=?,No_contact=?,Titre=?,Salaire=?,DateDembauche=?,Comission=?,Nodept=? WHERE No_employe=?";
+
+	public final static String DELETE_EMPLOYE_QUERY = "DELETE FROM employes WHERE No_employe=?";
+	
+	public final static String SELECT_ALL_EMPLOYES_QUERY = "SELECT No_employe,Nom,Prenom,NIC,DOB,Sexe,Adresse,AdresseEmail,No_contact,Titre,Salaire,DateDembauche,Comission,Nodept,Nom_dept\r\n" + 
+			" FROM employes E, departement D\r\n" + 
+			" WHERE E.Nodept = D.No_dept ORDER BY No_employe ASC";
+	
+	public final static String SELECT_DEPT_QUERY = "SELECT Nom_dept,No_dept FROM departement";
+
+	
+	
+	
+}
